6eb4181d3a658c7358c59b51eb9a50dafca30438
Oscar Almér <Oscar.almer94@gmail.com>
2020-11-22 12:04:15 +0100

    Created UTs

--- cpp/test/cpp_googletest_unittest/GildedRoseGoogletestUnitTests.cc
+++ cpp/test/cpp_googletest_unittest/GildedRoseGoogletestUnitTests.cc
@@ -6,5 +6,143 @@ TEST(GildedRoseTest, Foo) {
     items.push_back(Item("Foo", 0, 0));
     GildedRose app(items);
     app.updateQuality();
-    EXPECT_EQ("fixme", app.items[0].name);
+    EXPECT_EQ("Foo", app.items[0].name);
 }
+
+TEST(GildedRoseTest, Sulfuras)
+{
+    vector<Item> items;
+    int init_sellIn = 10;
+    int init_qual = 80;
+    items.push_back(Item("Sulfuras, Hand of Ragnaros", init_sellIn, init_qual));
+    GildedRose app(items);
+    for (int i = 0; i < 100; i++)
+    {
+        app.updateQuality();
+    }
+    EXPECT_EQ(init_qual, app.items[0].quality);
+    EXPECT_EQ(init_sellIn, app.items[0].sellIn);
+
+}
+
+TEST(GildedRoseTest, AgedBrie_Increasing_Value)
+{
+    vector<Item> items;
+    int init_sellIn = 10;
+    int init_qual = 30;
+    int num_days = 10;
+    items.push_back(Item("Aged Brie", init_sellIn, init_qual));
+    GildedRose app(items);
+    for (int i = 0; i < num_days; i++)
+    {
+        app.updateQuality();
+    }
+    EXPECT_EQ(app.items[0].quality, init_qual + num_days);
+}
+
+TEST(GildedRoseTest, AgedBrie_Max_Qual)
+{
+    vector<Item> items;
+    int init_sellIn = 10;
+    int init_qual = 50;
+    int num_days = 10;
+
+    items.push_back(Item("Aged Brie", init_sellIn, init_qual));
+    GildedRose app(items);
+    for (int i = 0; i < num_days; i++)
+    {
+        app.updateQuality();
+    }
+    
+    EXPECT_EQ(app.items[0].quality, GildedRose::max_quality);
+}
+
+TEST(GildedRoseTest, Backstage_Pass_Above_Double_Threshold)
+{
+    vector<Item> items;
+    int init_sellIn = 11;
+    int init_qual = 12;
+    int num_days = 1;
+
+    items.push_back(Item("Backstage passes to a TAFKAL80ETC concert", init_sellIn, init_qual));
+    GildedRose app(items);
+    for (int i = 0; i < num_days; i++)
+    {
+        app.updateQuality();
+    }
+
+    EXPECT_EQ(app.items[0].quality, init_qual + num_days);
+}
+
+TEST(GildedRoseTest, Backstage_Pass_On_Double_Threshold)
+{
+    vector<Item> items;
+    int init_sellIn = 10;
+    int init_qual = 11;
+    int num_days = 1;
+
+    items.push_back(Item("Backstage passes to a TAFKAL80ETC concert", init_sellIn, init_qual));
+    GildedRose app(items);
+    for (int i = 0; i < num_days; i++)
+    {
+        app.updateQuality();
+    }
+
+    EXPECT_EQ(app.items[0].quality, init_qual + 2*num_days);
+}
+
+TEST(GildedRoseTest, Backstage_Pass_Double_And_Tripple_Thresholds)
+{
+    vector<Item> items;
+    int init_sellIn = 10;
+    int init_qual = 11;
+    int num_days = 6;
+
+    items.push_back(Item("Backstage passes to a TAFKAL80ETC concert", init_sellIn, init_qual));
+    GildedRose app(items);
+    for (int i = 0; i < num_days; i++)
+    {
+        app.updateQuality();
+    }
+
+    EXPECT_EQ(app.items[0].quality, init_qual + 13);
+}
+
+
+TEST(GildedRoseTest, Backstage_Pass_Negative_Sellin)
+{
+    vector<Item> items;
+    int init_sellIn = 0;
+    int init_qual = 49;
+    int num_days = 1;
+
+    items.push_back(Item("Backstage passes to a TAFKAL80ETC concert", init_sellIn, init_qual));
+    GildedRose app(items);
+    for (int i = 0; i < num_days; i++)
+    {
+        app.updateQuality();
+    }
+
+    EXPECT_EQ(app.items[0].quality, GildedRose::min_quality);
+}
+
+TEST(GildedRoseTest, Backstage_Max_Quality_After_First_Update)
+{
+    vector<Item> items;
+    int init_sellIn = 5;
+    int init_qual = 48;
+    int num_days = 1;
+
+    items.push_back(Item("Backstage passes to a TAFKAL80ETC concert", init_sellIn, init_qual));
+    GildedRose app(items);
+    for (int i = 0; i < num_days; i++)
+    {
+        app.updateQuality();
+    }
+
+    EXPECT_EQ(app.items[0].quality, GildedRose::max_quality);
+}
+
+
+
+
