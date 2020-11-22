73db19128d0d4311f30edc8bf699d3f1db6d5244
Oscar Almér <Oscar.almer94@gmail.com>
2020-11-22 12:07:03 +0100

    Started refactoring of main function

--- cpp/src/GildedRose.cc
+++ cpp/src/GildedRose.cc
@@ -5,76 +5,131 @@ GildedRose::GildedRose(vector<Item> & items) : items(items)
     
 void GildedRose::updateQuality() 
 {
-    for (int i = 0; i < items.size(); i++)
+    for (Item & item : items)
     {
-        if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert")
+        int daily_quality_update = FindDailyQualityUpdate(item);
+        UpdateDueDate(item);
+
+        if (ItemPastDueDate(item))
         {
-            if (items[i].quality > 0)
+            if (isAgedBrie(item))
             {
-                if (items[i].name != "Sulfuras, Hand of Ragnaros")
-                {
-                    items[i].quality = items[i].quality - 1;
-                }
+                IncrementQuality(daily_quality_update);
             }
-        }
-        else
-        {
-            if (items[i].quality < 50)
+            else if (isConcertPass(item))
             {
-                items[i].quality = items[i].quality + 1;
-
-                if (items[i].name == "Backstage passes to a TAFKAL80ETC concert")
-                {
-                    if (items[i].sellIn < 11)
-                    {
-                        if (items[i].quality < 50)
-                        {
-                            items[i].quality = items[i].quality + 1;
-                        }
-                    }
-
-                    if (items[i].sellIn < 6)
-                    {
-                        if (items[i].quality < 50)
-                        {
-                            items[i].quality = items[i].quality + 1;
-                        }
-                    }
-                }
+                daily_quality_update = -item.quality;
             }
-        }
-
-        if (items[i].name != "Sulfuras, Hand of Ragnaros")
-        {
-            items[i].sellIn = items[i].sellIn - 1;
-        }
-
-        if (items[i].sellIn < 0)
-        {
-            if (items[i].name != "Aged Brie")
+            else if (isDecaying(item))
             {
-                if (items[i].name != "Backstage passes to a TAFKAL80ETC concert")
-                {
-                    if (items[i].quality > 0)
-                    {
-                        if (items[i].name != "Sulfuras, Hand of Ragnaros")
-                        {
-                            items[i].quality = items[i].quality - 1;
-                        }
-                    }
-                }
-                else
-                {
-                    items[i].quality = items[i].quality - items[i].quality;
-                }
+                DecrementQuality(daily_quality_update);
             }
             else
             {
-                if (items[i].quality < 50)
-                {
-                    items[i].quality = items[i].quality + 1;
-                }
+
+            } 
+        }
+        UpdateItemQuality(item, daily_quality_update);
+    }
+}
+
+int FindDailyQualityUpdate(const Item& item)
+{
+    int daily_quality_update = 0;
+    if (isMaturing(item))
+    {
+        IncrementQuality(daily_quality_update);
+
+        if (isConcertPass(item))
+        {
+            if (item.sellIn < 11)
+            {
+                IncrementQuality(daily_quality_update);
+            }
+
+            if (item.sellIn < 6)
+            {
+                IncrementQuality(daily_quality_update);
             }
         }
     }
+    else if (isDecaying(item))
+    {
+        DecrementQuality(daily_quality_update);
+    }
+
+    return daily_quality_update;
+}
+
+int GetQuality(const Item item)
+{
+    return item.quality;
+}
+
+void UpdateDueDate(Item& item)
+{
+    if (!isLegendary(item))
+    {
+        item.sellIn = item.sellIn - 1;
+    }
+}
+
+bool isMaturing(const Item& item)
+{
+    return isAgedBrie(item) || isConcertPass(item);
+}
+
+bool isDecaying(const Item& item)
+{
+    return !isLegendary(item) && !isAgedBrie(item) && !isConcertPass(item);
+}
+
+bool isAgedBrie(const Item& item)
+{
+    return "Aged Brie" == item.name;
+}
+
+bool isConcertPass(const Item& item)
+{
+    return "Backstage passes to a TAFKAL80ETC concert" == item.name;
+}
+
+void IncrementQuality(int & daily_quality_update)
+{
+    daily_quality_update = daily_quality_update+ 1;
+}
+
+void DecrementQuality(int & daily_quality_update)
+{
+    daily_quality_update = daily_quality_update - 1;
+}
+
+bool ItemPastDueDate(const Item& item)
+{
+    return item.sellIn < 0;
+}
+
+void UpdateItemQuality(Item& item, const int daily_quality_update)
+{
+    if (!isLegendary(item))
+    {
+        int clamped_quality = ClampItemQuality(item.quality + daily_quality_update);
+        item.quality = clamped_quality;
+    }
+}
+
+int ClampItemQuality(const int total_quality)
+{
+    int clamped_quality = std::min(std::max(GildedRose::min_quality, total_quality), GildedRose::max_quality);
+    return clamped_quality;  
+}
+
+bool isLegendary(const Item& item)
+{
+    return "Sulfuras, Hand of Ragnaros" == item.name;
+}
+
+bool IsConjured(const Item& item)
+{
+    return item.name.find("Conjured") != std::string::npos;
 }
--- cpp/src/GildedRose.h
+++ cpp/src/GildedRose.h
@@ -18,7 +18,23 @@ class GildedRose
 public:
     vector<Item> & items;
     GildedRose(vector<Item> & items);
-    
-    void updateQuality();
+    static int const min_quality = 0;
+    static int const max_quality = 50;
+
+    void updateQuality();   
 };
 
+int FindDailyQualityUpdate(const Item& item);
+int GetQuality(const Item item);
+void UpdateDueDate(Item& item);
+bool isMaturing(const Item& item);
+bool isDecaying(const Item& item);
+bool isAgedBrie(const Item& item);
+bool isConcertPass(const Item& item);
+void IncrementQuality(int & daily_quality_update);
+void DecrementQuality(int & daily_quality_update);
+bool ItemPastDueDate(const Item& item);
+void UpdateItemQuality(Item& item, const int daily_quality_update);
+int ClampItemQuality(int total_quality);
+bool isLegendary(const Item& item);
+bool IsConjured(const Item& item);
